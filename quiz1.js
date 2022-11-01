127b8688fdbc6b3a07b14878eb91cc0717adee31
Budhathoki, Karan <budhathokikaran135@gmail.com>
2022-10-31 19:42:42 -0500

    Updated file to save quizes result

--- quiz1.js
+++ quiz1.js
@@ -1,3 +1,4 @@
+
 const quizData = [
     {
         question: "What does HTML stand for?",
@@ -83,3 +84,9 @@ submitBtn.addEventListener('click', () => {
        }
     }
 })
+    
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    

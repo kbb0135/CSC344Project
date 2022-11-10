c6f7ac9b889c67f1cbdb2f88309fe9032b8a2b8c
PremAdhikari10 <103157060+PremAdhikari10@users.noreply.github.com>
2022-11-09 23:26:24 -0600

    File Added for Progress Report

--- progressbar.js
+++ progressbar.js
@@ -1,11 +1,31 @@
-function start(i){
+let q1 = 0;
+let q2 =1;
+let q3 = 2;
+let quiz1;
+function start(q1,quiz1) {
 
-        var percent = document.querySelector(`#progress_${i} .percent`);
-        var progressing = document.querySelector(`#progress_${i} .percent`);
-        var circle = document.querySelector(`#progress_${i} .percent`);
-
+        var percent = document.querySelector(`#progress_${q1} .percent`);
+        var progressing = document.querySelector(`#progress_${q1} .progressing`);
+        var circle = document.querySelector(`#progress_${q1} .circle`);
+    
         requestAnimationFrame(startProgress);
-
         var progress = 0;
-        var random  = parseInt(Math.()*100);
-}
+       
+        totalScore1 = quiz1*25;
+        var random = totalScore1;
+        console.log(totalScore1);
+        console.log(random);
+        function startProgress() {
+            progress += 1;
+            if (progress <= random) {
+                percent.innerHTML = parseInt(progress) + '%';
+                progressing.style.width = progress + '%';
+                circle.style.left = progress + '%';
+            }
+            requestAnimationFrame(startProgress);
+        }
+    
+    }
+    
+    start(q1);
+    start(q2);

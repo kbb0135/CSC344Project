e7437ed558f6542fed650f2c32a385cf9fb6d5ad
Charan Niroula <charanniroula02@gmail.com>
2022-11-10 18:14:35 -0600

    flashcard

--- default_flashcard.js
+++ default_flashcard.js
@@ -1,18 +1,22 @@
 export const cardData = [
     {
         question: "What is my name?",
-        answer: "Charan"
+        answer: "Charan",
     },
     {
         question: "What is Prem's name?",
-        answer: "Prem"
+        answer: "Prem",
     },
 ];
 
 const card_question = document.getElementById('question')
 const card_answer = document.getElementById('answer')
+const next_btn = document.getElementById('next')
+const previous_btn = document.getElementById('previous')
 
 let currentCard = 0
+
+loadCard()
 
 function loadCard() {
     const currentCardData = cardData[currentCard]
@@ -20,3 +24,19 @@ function loadCard() {
     card_question.innerText = currentCardData.question
     card_answer.innerText = currentCardData.answer
 }
+
+next_btn.addEventListener("click", () => {
+    currentCard++
+
+    if(currentCard < cardData.length) {
+        loadCard()
+    }
+})
+
+previous_btn.addEventListener("click", () => {
+    currentCard--;
+
+    if(currentCard < cardData.length) {
+        loadCard()
+    }
+})
